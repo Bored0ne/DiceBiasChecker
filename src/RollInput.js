ad616fde9f8ab800e1934a8d1bb903a65cf495fe
Stephen Seaton-Blanchard <stephen@seatoncode.com>
2020-11-26 01:57:51 -0600

    Trusting the mobile library

--- src/RollInput.js
+++ src/RollInput.js
@@ -1,50 +1,50 @@
 import React, {useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import {MOBILE_SIZE} from "./index";
 
 const InputContainer = styled.div`
  display: flex;
  flex-flow: row nowrap;
- @media(max-width: 650px){
+ ${({isMobile}) => isMobile && `  
   flex-flow: column nowrap; 
- }
+ `}
 `;
 const ResponsiveInput = styled.input`
-  @media(max-width: 650px){
+  ${({isMobile}) => isMobile && `
     width: 95vw;
     height: 5vh;
-  }
+  `}
 `;
 const ResponsiveButton = styled.button`
-  @media(max-width: 650px){
+  ${({isMobile}) => isMobile && `
     height: 5vh;
     font-size: 4vh;
     //width: 100vw;
-  }
+  `}
 `
 const ResponsiveTableContainer = styled.div`
-  @media(max-width: 650px){
+  width: 100%;
+  height: fit-content;
+  ${({isMobile}) => isMobile && `
     max-height: 40vh;
     width: 100%;
     overflow-y: scroll;
-  }
-  width: 100%;
-  height: fit-content;
+  `}
 `
 const BorderedTD = styled.td`
 border: 1px solid gray;
 `;
 
 const ResponsiveTable = styled.table`
- @media(max-width: 650px){
-  font-size: 4vh;
-  width: 100%;
- }
   text-align: center;
+  ${({isMobile}) => isMobile && `
+    font-size: 4vh;
+    width: 100%;
+ `}
+  
 `
 
-const RollInput = ({update}) => {
+const RollInput = ({update, isMobile}) => {
   let [rolls, setRolls] = useState([]);
   useEffect(() => {
     update(rolls);
@@ -67,23 +67,32 @@ const RollInput = ({update}) => {
   }
 
   return <div>
-    <InputContainer>
-      <ResponsiveInput type="number"
-                       value={currentRoll}
-                       onChange={({target}) => {
-                         setCurrentRoll(target.value)
-                       }}
-                       onKeyDown={event => {
-                         if (event.keyCode == '13') {
-                           add()
-                         }
-                       }}
+    <InputContainer
+      isMobile={isMobile}>
+      <ResponsiveInput
+        isMobile={isMobile}
+        type="number"
+        value={currentRoll}
+        onChange={({target}) => {
+          setCurrentRoll(target.value)
+        }}
+        onKeyDown={event => {
+          if (event.keyCode == '13') {
+            add()
+          }
+        }}
       />
-      <ResponsiveButton onClick={add}>add</ResponsiveButton>
-      <ResponsiveButton onClick={clear}>clear</ResponsiveButton>
+      <ResponsiveButton
+        isMobile={isMobile}
+        onClick={add}>add</ResponsiveButton>
+      <ResponsiveButton
+        isMobile={isMobile}
+        onClick={clear}>clear</ResponsiveButton>
     </InputContainer>
-    <ResponsiveTableContainer>
-      <ResponsiveTable>
+    <ResponsiveTableContainer
+      isMobile={isMobile}>
+      <ResponsiveTable
+        isMobile={isMobile}>
         <thead>
         <th>roll</th>
         <th>delete</th>
@@ -91,11 +100,14 @@ const RollInput = ({update}) => {
         <tbody>
         {rolls.map((roll, index) => {
           return <tr key={index}>
-            <BorderedTD>{roll}</BorderedTD>
+            <BorderedTD
+              isMobile={isMobile}>{roll}</BorderedTD>
             <BorderedTD>
-              <ResponsiveButton onClick={() => {
-                deleteRoll(index);
-              }}>X
+              <ResponsiveButton
+                isMobile={isMobile}
+                onClick={() => {
+                  deleteRoll(index);
+                }}>X
               </ResponsiveButton>
             </BorderedTD>
           </tr>
--- src/index.js
+++ src/index.js
@@ -4,24 +4,25 @@ import ReactDom from 'react-dom';
 import {DiceGraph} from "./DiceGraph";
 import {RollInput} from "./RollInput";
 import styled from 'styled-components';
-
+import {isMobile} from 'react-device-detect';
 // export const MOBILE_SIZE = 650;
 
 const Container = styled.div`
   display:flex;
   flex-flow: row;
   width:100%;
-  @media(max-width: 650px){
+  ${({isMobile}) => isMobile && `  
     flex-flow: column;
     align-items: center;
-  }
+  `}
 `;
 
 const Index = ({...props}) => {
   let [rolls, setRolls] = useState([]);
-  return <Container>
-    <RollInput update={setRolls}/>
-    <DiceGraph rolls={rolls}/>
+
+  return <Container isMobile={isMobile}>
+    <RollInput update={setRolls} isMobile={isMobile}/>
+    <DiceGraph rolls={rolls} isMobile={isMobile}/>
   </Container>;
 };
 
