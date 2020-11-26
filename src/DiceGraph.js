7af1e4df1f6de5ce545e20de49ab9a4fc6102f2a
Stephen Seaton-Blanchard <stephen@seatoncode.com>
2020-11-26 14:25:20 -0600

    Adding guide lines

--- src/DiceGraph.js
+++ src/DiceGraph.js
@@ -22,14 +22,40 @@ const DiceGraph = ({rolls = [2, 2, 3, 4, 5, 2, 2, 2, 2, 2, 2, 2, 2]}) => {
             label: 'Actual rolls',
             data: Object.values(countOfGroupedRolls),
             borderColor: 'rgb(0, 0, 255)',
-            backgroundColor: 'rgba(0, 0, 255, .25)'
+            // backgroundColor: 'rgba(0, 0, 255, .25)'
           },
           {
-            label: 'expected average',
-            data: expectedAverageOfRolls,
-            // type: 'line',
+            label: '10% cap',
+            data: expectedAverageOfRolls.map(val => val + (val * 10 / 100)),
+            borderColor: 'green',
+            // backgroundColor: 'rgba(255, 0, 0, .25)'
+          },
+          {
+            label: '10% floor',
+            data: expectedAverageOfRolls.map(val => val - (val * 10 / 100)),
+            borderColor: 'green',
+          },
+          {
+            label: '20% cap',
+            data: expectedAverageOfRolls.map(val => val + (val * 20 / 100)),
+            borderColor: 'yellow',
+            // backgroundColor: 'rgba(255, 0, 0, .25)'
+          },
+          {
+            label: '20% floor',
+            data: expectedAverageOfRolls.map(val => val - (val * 20 / 100)),
+            borderColor: 'yellow',
+          },
+          {
+            label: '30% cap',
+            data: expectedAverageOfRolls.map(val => val + (val * 30 / 100)),
             borderColor: 'red',
-            backgroundColor: 'rgba(255, 0, 0, .25)'
+            // backgroundColor: 'rgba(255, 0, 0, .25)'
+          },
+          {
+            label: '30% floor',
+            data: expectedAverageOfRolls.map(val => val - (val * 30 / 100)),
+            borderColor: 'red',
           }
         ],
         labels: Object.getOwnPropertyNames(countOfGroupedRolls)
